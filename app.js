51112643e1a7a068de7ddb3a1ec2af9256939f42
Harold Beckerman <halbeckerman@yahoo.com>
2019-04-01 12:14:57 -0500

    returns a specific number of meeting per user input

--- app.js
+++ app.js
@@ -9,9 +9,24 @@ function append(parent, el) {
 }
 
 //Helper variables
+let meetings;
+let numOfMeetings;
+const numBox = document.getElementById('number-of-meetings');
 const ul = document.getElementById('meetings');
 const url = 'http://aa-ksdist23.org/wp/wp-admin/admin-ajax.php?action=meetings';
 const start = new Date();
+
+//set some event listeners
+numBox.addEventListener('change', e => {
+  e.preventDefault();
+  buildMeetings();
+});
+
+document.getElementById('go').addEventListener('click', e => {
+  e.preventDefault();
+  buildMeetings();
+});
+
 
 const today = start.getDay();
 const weekdays = new Array(7); //Create an array to use for rendering day index
@@ -28,15 +43,28 @@ const timeNow = start.toTimeString("hh/mm");
 
 console.log(dayNow);
 console.log(timeNow);
-
 //Fetch, bitch
 fetch(url)
 .then((resp) => resp.json())
 .then(function(data) {
-  const meetings = data; //A nice name for what the data is
-  let weekdayToday = weekdays[today]; //Parse the day number to a string
+  meetings = data; //A nice name for what the data is
+  numBox.setAttribute('max', meetings.length)
+  buildMeetings();
+})
+.catch(function(error) {
+  console.log(JSON.stringify(error));
+});
 
-  return meetings.map(function(meeting) { //The data is an array so we can map it
+function buildMeetings() {
+  //the if statement may be unnecessary
+  if(ul.hasChildNodes()){
+    while(ul.hasChildNodes()){
+      ul.removeChild(ul.lastChild);
+    };
+  };
+  numOfMeetings = Number(numBox.value);
+  let weekdayToday = weekdays[today]; //Parse the day number to a string
+  return filterByNow().map(function(meeting) { //The data is an array so we can map it
     const meetingTime = meeting.time_formatted;
     const d = meeting.day;
     const meetingDay = weekdays[d];
@@ -49,7 +77,7 @@ fetch(url)
         url = createNode('a');
 
     url.href = meeting.url;    //Assign the href to new element using JSON url
-
+    //I love template literals. Just fucking love 'em
     name.innerHTML = `${meeting.name}`;  //Interpolation for each element
     day.innerHTML = `${meetingDay}`;
     time.innerHTML = `${meetingTime}`;
@@ -66,7 +94,23 @@ fetch(url)
     append(li, url);
     append(ul, li);
   })
-})
-.catch(function(error) {
-  console.log(JSON.stringify(error));
-});
+};
+
+function filterByNow(){
+  const myTime = Number(`${start.getHours()}${start.getMinutes()}`)
+  let nextMeetingIndex = meetings.findIndex(meeting => Number(meeting.day)===today && Number(meeting.time.split(':').join(''))>myTime)
+  // returns -1 if there are no more meetings that day
+  if(nextMeetingIndex===-1){
+    if(today===6){
+      nextMeetingIndex = 0;
+    } else {
+      nextMeetingIndex = meetings.findIndex(meeting => Number(meeting.day)===today+1)
+    }
+  }
+  let upcomingMeetings = meetings.slice(nextMeetingIndex, nextMeetingIndex+numOfMeetings);
+  if(upcomingMeetings.length<numOfMeetings){
+    upcomingMeetings=[...upcomingMeetings, array.slice(0, numOfMeetings-upcomingMeetings.length)]
+  }
+  console.log(upcomingMeetings.length)
+  return upcomingMeetings;
+};
